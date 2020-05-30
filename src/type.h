90a0b9bcc94b84e2fba7c43dac99a89f2908b7fe
Michael Koller <koller_michael@outlook.de>
2020-05-30 22:08:58 +0200

    Update type.h

--- src/type.h
+++ src/type.h
@@ -5,4 +5,5 @@
 
 const std::uint8_t MAX_COMPONENTS = 4;
 using ComponentBitField = std::bitset<MAX_COMPONENTS>;
-using ComponentType = std::uint64_t;
+using ComponentTypeId = std::uint64_t;
+using EntityIdType = std::uint64_t;
