a8d7c0bccf6c31658afa3c1015107987e2c0ec06
Michael Koller <koller_michael@outlook.de>
2020-06-08 21:16:29 +0200

    Update test_entity_manager: more tests; test RemoveComponent

--- src/entity_manager.h
+++ src/entity_manager.h
@@ -33,7 +33,7 @@ public:
         const std::string& type_name = typeid(T).name();
 
         // assert that component type has not already been registered
-        assert(component_type_id_mapper_.find(type_name) == component_type_id_mapper_.end());
+        assert(component_type_id_mapper_.find(type_name) == component_type_id_mapper_.end() && "Component type already registered.");
         // register by assigning an ID to the new component type
         component_type_id_mapper_.insert({type_name, next_component_type_id_});
 
@@ -53,16 +53,19 @@ public:
     template <typename T>
     void AddComponent(Entity entity, T component)
     {
+        auto ecm = GetComponentMap<T>();
+        // assert that entity does not already have this component type
+        assert(ecm->entity_component_map_.find(entity) == ecm->entity_component_map_.end() && "Entity already has this component.");
+        ecm->entity_component_map_.insert({entity, component});
         // set the bit corresponding to the component type to indicate that this entity now "has" the component
         entity_component_bitfield_[entity].set(TypeIdOf<T>());
-        GetComponentMap<T>()->entity_component_map_.insert({entity, component});
     }
 
     template <typename T>
     T& GetComponent(Entity entity)
     {
         // assert that entity has component T (by checking if the corresponding bit is set)
-        assert(entity_component_bitfield_[entity][TypeIdOf<T>()]);
+        assert(entity_component_bitfield_[entity][TypeIdOf<T>()] && "Entity does not have this component.");
         return GetComponentMap<T>()->entity_component_map_[entity];
     }
 
@@ -70,10 +73,10 @@ public:
     template <typename... T>
     typename std::enable_if<sizeof...(T) == 0, ComponentBitField>::type ComponentBitFieldOf()
     {
-        ComponentBitField cbf;
-        return cbf;
+        return ComponentBitField();
     }
 
+    // get a bit field where the bits corresponding to the template arguments are set
     template <typename T, typename... Args>
     ComponentBitField ComponentBitFieldOf()
     {
@@ -85,12 +88,19 @@ public:
     template <typename T>
     void RemoveComponent(Entity entity, T component)
     {
+        RemoveComponent<T>(entity);
+    }
+
+    template <typename T>
+    void RemoveComponent(Entity entity)
+    {
+        // assert that entity hast this component type
+        assert(entity_component_bitfield_[entity][TypeIdOf<T>()] && "Entity does not have this component.");
         // reset the bit corresponding to the component type to indicate that this entity no longer "has" the component
         entity_component_bitfield_[entity].reset(TypeIdOf<T>());
         GetComponentMap<T>()->entity_component_map_.erase(entity);
     }
 
-private:
     // cast from base class (IComponentMap) to derived class (ComponentMap)
     template <typename T>
     std::shared_ptr<ComponentMap<T>> GetComponentMap()
@@ -98,6 +108,7 @@ private:
         return std::static_pointer_cast<ComponentMap<T>>(component_map_[TypeIdOf<T>()]);
     }
 
+private:
     int existing_entities_count_;
     EntityIdType next_entity_id_;
     ComponentTypeId next_component_type_id_;
--- unittests/test_entity_manager.cc
+++ unittests/test_entity_manager.cc
@@ -7,11 +7,25 @@
 #include "entity_manager.h"
 #include "type.h"
 
-BOOST_AUTO_TEST_CASE( create_destroy_entities )
+struct TestComponent0
+{
+    int a = -1;
+};
+struct TestComponent1
+{
+    float b = -1;
+};
+struct TestComponent2
+{
+    bool c = false;
+};
+
+BOOST_AUTO_TEST_CASE( create_and_destroy_entities )
 {
     EntityManager entity_manager;
 
-    // create some entities and check if the counter is correct
+    /* create some entities and check if the counter is correct */
+
     Entity ent1 = entity_manager.CreateEntity();
     BOOST_CHECK_EQUAL(entity_manager.GetExistingEntitiesCount(), 1);
 
@@ -21,14 +35,16 @@ BOOST_AUTO_TEST_CASE( create_destroy_entities )
     Entity ent3 = entity_manager.CreateEntity();
     BOOST_CHECK_EQUAL(entity_manager.GetExistingEntitiesCount(), 3);
 
-    // destroy some entities and check if the counter is correct
+    /* destroy some entities and check if the counter is correct */
+
     entity_manager.DestroyEntity(ent1);
     BOOST_CHECK_EQUAL(entity_manager.GetExistingEntitiesCount(), 2);
 
     entity_manager.DestroyEntity(ent2);
     BOOST_CHECK_EQUAL(entity_manager.GetExistingEntitiesCount(), 1);
 
-    // create new entities (reusing destroyed ones)
+    /* create new entities (reusing destroyed ones) */
+
     Entity ent4 = entity_manager.CreateEntity();
     BOOST_CHECK_EQUAL(entity_manager.GetExistingEntitiesCount(), 2);
 
@@ -39,24 +55,10 @@ BOOST_AUTO_TEST_CASE( create_destroy_entities )
     BOOST_CHECK_EQUAL(entity_manager.GetExistingEntitiesCount(), 4);
 }
 
-BOOST_AUTO_TEST_CASE( register_add_get_components )
+BOOST_AUTO_TEST_CASE( register_add_and_remove_components )
 {
-    struct TestComponent0
-    {
-        int a = -1;
-    };
-
-    struct TestComponent1
-    {
-        float b = -1;
-    };
-
-    struct TestComponent2
-    {
-        bool c = false;
-    };
-
     /* register components and check assigned ID */
+
     EntityManager entity_manager;
     entity_manager.RegisterComponent<TestComponent0>();
     entity_manager.RegisterComponent<TestComponent1>();
@@ -66,6 +68,7 @@ BOOST_AUTO_TEST_CASE( register_add_get_components )
     BOOST_CHECK_EQUAL(entity_manager.TypeIdOf<TestComponent2>(), 2);
 
     /* add components to entities and check bit field entries */
+
     // entity without components
     Entity ent0 = entity_manager.CreateEntity();
     ComponentBitField bitfield0 = entity_manager.GetBitField(ent0);
@@ -90,16 +93,54 @@ BOOST_AUTO_TEST_CASE( register_add_get_components )
     expected_bitfield2.set(2);
     BOOST_CHECK_EQUAL(bitfield2, expected_bitfield2);
 
-    // entity with three components (by adding one more component to ent2)
-    entity_manager.AddComponent(ent2, TestComponent1{21});
-    ComponentBitField bitfield3 = entity_manager.GetBitField(ent2);
+    // entity with three components
+    Entity ent3 = entity_manager.CreateEntity();
+    entity_manager.AddComponent(ent3, TestComponent0{20});
+    entity_manager.AddComponent(ent3, TestComponent1{21});
+    entity_manager.AddComponent(ent3, TestComponent2{true});
+    ComponentBitField bitfield3 = entity_manager.GetBitField(ent3);
     ComponentBitField expected_bitfield3;
     expected_bitfield3.set(0);
     expected_bitfield3.set(1);
     expected_bitfield3.set(2);
     BOOST_CHECK_EQUAL(bitfield3, expected_bitfield3);
 
+    /* remove components from entities and check bit field entries */
+
+    // remove the only component
+    entity_manager.RemoveComponent<TestComponent2>(ent1);
+    bitfield1 = entity_manager.GetBitField(ent1);
+    BOOST_CHECK_EQUAL(bitfield1, ComponentBitField());
+
+    // remove one of two components
+    entity_manager.RemoveComponent<TestComponent0>(ent2);
+    bitfield2 = entity_manager.GetBitField(ent2);
+    expected_bitfield2.reset(0);
+    BOOST_CHECK_EQUAL(bitfield2, expected_bitfield2);
+
+    // remove two of three components
+    entity_manager.RemoveComponent<TestComponent0>(ent3);
+    bitfield3 = entity_manager.GetBitField(ent3);
+    expected_bitfield3.reset(0);
+    BOOST_CHECK_EQUAL(bitfield3, expected_bitfield3);
+    entity_manager.RemoveComponent<TestComponent1>(ent3);
+    bitfield3 = entity_manager.GetBitField(ent3);
+    expected_bitfield3.reset(1);
+    BOOST_CHECK_EQUAL(bitfield3, expected_bitfield3);
+}
+
+BOOST_AUTO_TEST_CASE( get_components_and_entity_component_map )
+{
+    EntityManager entity_manager;
+    entity_manager.RegisterComponent<TestComponent0>();
+    entity_manager.RegisterComponent<TestComponent1>();
+    entity_manager.RegisterComponent<TestComponent2>();
+    BOOST_CHECK_EQUAL(entity_manager.TypeIdOf<TestComponent0>(), 0);
+    BOOST_CHECK_EQUAL(entity_manager.TypeIdOf<TestComponent1>(), 1);
+    BOOST_CHECK_EQUAL(entity_manager.TypeIdOf<TestComponent2>(), 2);
+
     /* get component bit fields */
+
     // bit field without components
     ComponentBitField cbf = entity_manager.ComponentBitFieldOf<>();
     ComponentBitField expected_cbf;
@@ -126,22 +167,43 @@ BOOST_AUTO_TEST_CASE( register_add_get_components )
     expected_cbf.set(2);
     BOOST_CHECK_EQUAL(cbf, expected_cbf);
 
-    /* get components */
+    /* get components and check entity component map */
+
+    // size of entity component maps must be 0 when no components have been added
+    auto cm_tc0 = entity_manager.GetComponentMap<TestComponent0>();
+    auto cm_tc1 = entity_manager.GetComponentMap<TestComponent1>();
+    auto cm_tc2 = entity_manager.GetComponentMap<TestComponent2>();
+    BOOST_CHECK_EQUAL(cm_tc0->entity_component_map_.size(), 0);
+    BOOST_CHECK_EQUAL(cm_tc1->entity_component_map_.size(), 0);
+    BOOST_CHECK_EQUAL(cm_tc2->entity_component_map_.size(), 0);
+
     // entity with one component
     Entity ent1comp = entity_manager.CreateEntity();
     entity_manager.AddComponent(ent1comp, TestComponent0{123});
+    BOOST_CHECK_EQUAL(cm_tc0->entity_component_map_.size(), 1);
     auto& e1tc0 = entity_manager.GetComponent<TestComponent0>(ent1comp);
     BOOST_CHECK_EQUAL(e1tc0.a, 123);
-    // modify component
-    e1tc0.a *= 2;
+    e1tc0.a *= 2;   // modify component
     BOOST_CHECK_EQUAL(entity_manager.GetComponent<TestComponent0>(ent1comp).a, 246);
 
     // entity with two components
     Entity ent2comp = entity_manager.CreateEntity();
-    entity_manager.AddComponent(ent2comp, TestComponent0{123});
+    TestComponent0 tc0 = TestComponent0{123};
+    entity_manager.AddComponent(ent2comp, tc0);
+    BOOST_CHECK_EQUAL(cm_tc0->entity_component_map_.size(), 2);
     entity_manager.AddComponent(ent2comp, TestComponent2{true});
+    BOOST_CHECK_EQUAL(cm_tc2->entity_component_map_.size(), 1);
     auto& e2tc0 = entity_manager.GetComponent<TestComponent0>(ent2comp);
     auto& e2tc2 = entity_manager.GetComponent<TestComponent2>(ent2comp);
     BOOST_CHECK_EQUAL(e2tc0.a, 123);
     BOOST_CHECK_EQUAL(e2tc2.c, true);
+
+    /* remove components and check entity component map */
+
+    entity_manager.RemoveComponent<TestComponent0>(ent1comp);
+    BOOST_CHECK_EQUAL(cm_tc0->entity_component_map_.size(), 1);
+    entity_manager.RemoveComponent(ent2comp, tc0);  // test the other version of RemoveComponent
+    BOOST_CHECK_EQUAL(cm_tc0->entity_component_map_.size(), 0);
+    entity_manager.RemoveComponent<TestComponent2>(ent2comp);
+    BOOST_CHECK_EQUAL(cm_tc2->entity_component_map_.size(), 0);
 }
